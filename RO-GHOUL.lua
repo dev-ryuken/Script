5fe8464a8d30d2ccad51e4435cc0e68452d908b4
dev-ryuken <toukakrishima56@gmail.com>
2024-10-26 22:44:12 +0800

    Update RO-GHOUL.lua

--- RO-GHOUL.lua
+++ RO-GHOUL.lua
@@ -484,7 +484,7 @@ while true do
                     labels("text", "Moving to: "..npc.Name)
 
                     if myData.Boss[npc.Name] or npc.Parent.Name == "GyakusatsuSpawn" then
-                        tp(npc.HumanoidRootPart.CFrame * CFrame.Angles(math.rad(180),0,0) + Vector3.new(0,myData.DistanceFromBoss,0))
+                        tp(npc.HumanoidRootPart.CFrame * CFrame.Angles(math.rad(0),180,180) + Vector3.new(0,myData.DistanceFromBoss,0))
                     else
                         tp(npc.HumanoidRootPart.CFrame + npc.HumanoidRootPart.CFrame.lookVector * myData.DistanceFromNpc)
                     end
